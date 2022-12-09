66a631f5794735a64da5f253ea61683fff47779d
jrikula <jrikula@gmail.com>
2022-12-09 14:12:20 +0200

    Egg destruction added

--- ChickenBoyAdventure/Assets/scripts/Spawn.cs
+++ ChickenBoyAdventure/Assets/scripts/Spawn.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject objToSpawn;
+    public GameObject original;
     public Transform platformPosition;
 
     TouchingDirections touchingDirections;
 
-    // Start is called before the first frame update
-    private void Awake()
+    void Awake()
     {
         touchingDirections = GetComponent<TouchingDirections>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetButtonDown("Jump") && !touchingDirections.IsGrounded)
+        if (Input.GetButtonDown("Jump") && !touchingDirections.IsGrounded)
         {
-            Instantiate(objToSpawn, platformPosition.position, platformPosition.rotation);
+            GameObject clone = (GameObject)Instantiate(original, platformPosition.position, platformPosition.rotation);
+            if (!touchingDirections.IsGrounded)
+            {
+                Destroy(clone, 5f);
+            }
         }
     }
 }
